7fd22f4740d109ca425ece25d434c0302fd1c372
Perkova Anastasiya <44846835+anastasiyaperk@users.noreply.github.com>
2022-04-08 19:47:04 +0500

    Небольшой рефакторинг

--- compare_serialization_formats.py
+++ compare_serialization_formats.py
@@ -8,56 +8,57 @@ from timeit import timeit
 
 import pandas as pd
 
-if __name__ == '__main__':
-    test_data = {
-        "string_data": r"Lorem ipsum dolor sit amet, consectetur adipiscing"
-                       "elit. Mauris adipiscing adipiscing placerat."
-                       "Vestibulum augue augue, pellentesque quis sollicitudin id, adipiscing.",
-        "array_data": [i for i in range(100)],
-        "dict_data": {str(x): x + 1 for x in range(100)},
-        "int_data": 123,
-        "float_data": 123.09,
-    }
+test_data = {
+    "string_data": r"Lorem ipsum dolor sit amet, consectetur adipiscing"
+                   "elit. Mauris adipiscing adipiscing placerat."
+                   "Vestibulum augue augue, pellentesque quis sollicitudin id, adipiscing.",
+    "array_data": [i for i in range(100)],
+    "dict_data": {str(x): x + 1 for x in range(100)},
+    "int_data": 123,
+    "float_data": 123.09,
+}
 
-    setup_pickle = f"import pickle ; " \
-                   f"d={test_data} ;  " \
-                   f"src = pickle.dumps(d, 2)"
+setup_pickle = f"import pickle ; " \
+               f"d={test_data} ;  " \
+               f"src = pickle.dumps(d, 2)"
 
-    setup_json = f"import json; " \
-                 f"d={test_data} ; " \
-                 f"src = json.dumps(d)"
+setup_json = f"import json; " \
+             f"d={test_data} ; " \
+             f"src = json.dumps(d)"
 
-    setup_yaml = f"import yaml; " \
-                 f"d={test_data} ; " \
-                 f"src = yaml.dump(d)"
+setup_yaml = f"import yaml; " \
+             f"d={test_data} ; " \
+             f"src = yaml.dump(d)"
 
-    setup_xml = f"from dicttoxml import dicttoxml;" \
-                f"from xml.dom.minidom import parseString; " \
+setup_xml = f"from dicttoxml import dicttoxml;" \
+            f"from xml.dom.minidom import parseString; " \
+            f"d={test_data}; " \
+            f"src = dicttoxml(d)"
+
+setup_proto = f"from google.protobuf import json_format; " \
+              f"from google.protobuf.struct_pb2 import Struct; " \
+              f"d={test_data}; " \
+              f"src = Struct(); " \
+              f"src.update(d)"
+
+setup_msgpack = f"import msgpack; " \
                 f"d={test_data}; " \
-                f"src = dicttoxml(d)"
+                f"src = msgpack.packb(d); "
 
-    setup_proto = f"from google.protobuf import json_format; " \
-                  f"from google.protobuf.struct_pb2 import Struct; " \
-                  f"d={test_data}; " \
-                  f"src = Struct(); " \
-                  f"src.update(d)"
+# (title, setup, enc_test, dec_test)
+tests = [
+    ('[pickle (native)]', setup_pickle, 'pickle.dumps(d, 2)', 'pickle.loads(src)'),
+    ('[json]', setup_json, 'json.dumps(d)', 'json.loads(src)'),
+    ('[yaml]', setup_yaml, 'yaml.dump(d)', 'yaml.safe_load(src)'),
+    ('[xml]', setup_xml, 'dicttoxml(d)', 'parseString(src)'),
+    ('[protobuf]', setup_proto, 's = Struct(); s.update(d)',
+     'json_format.MessageToDict(src)'),
+    ('[msgpack]', setup_msgpack, 'msgpack.packb(d)', 'msgpack.unpackb(src)'),
+]
 
-    setup_msgpack = f"import msgpack; " \
-                    f"d={test_data}; " \
-                    f"src = msgpack.packb(d); "
 
-    # (title, setup, enc_test, dec_test)
-    tests = [
-        ('[pickle (native)]', setup_pickle, 'pickle.dumps(d, 2)', 'pickle.loads(src)'),
-        ('[json]', setup_json, 'json.dumps(d)', 'json.loads(src)'),
-        ('[yaml]', setup_yaml, 'yaml.dump(d)', 'yaml.safe_load(src)'),
-        ('[xml]', setup_xml, 'dicttoxml(d)', 'parseString(src)'),
-        ('[protobuf]', setup_proto, 's = Struct(); s.update(d)',
-         'json_format.MessageToDict(src)'),
-        ('[msgpack]', setup_msgpack, 'msgpack.packb(d)', 'msgpack.unpackb(src)'),
-    ]
-
-    loops = 100
+if __name__ == '__main__':
+    loops = 1000
     enc_table = []
     dec_table = []
     src = None
